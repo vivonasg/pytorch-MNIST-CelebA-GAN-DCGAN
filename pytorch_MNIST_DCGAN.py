a99b0f1f09d142e7f036bda5268bed0e92232a38
vivonasg <>
2018-04-19 17:08:41 -0400

    make mnist dcgan able to run on vm

--- pytorch_MNIST_DCGAN.py
+++ pytorch_MNIST_DCGAN.py
@@ -76,7 +76,8 @@ def normal_init(m, mean, std):
 
 fixed_z_ = torch.randn((5 * 5, 100)).view(-1, 100, 1, 1)    # fixed noise
 fixed_z_ = Variable(fixed_z_.cuda(), volatile=True)
-def show_result(num_epoch, show = False, save = False, path = 'result.png', isFix=False):
+
+def save_result(path = 'result.png', isFix=False):
     z_ = torch.randn((5*5, 100)).view(-1, 100, 1, 1)
     z_ = Variable(z_.cuda(), volatile=True)
 
@@ -87,50 +88,9 @@ def show_result(num_epoch, show = False, save = False, path = 'result.png', isFi
         test_images = G(z_)
     G.train()
 
-    size_figure_grid = 5
-    fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(5, 5))
-    for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
-        ax[i, j].get_xaxis().set_visible(False)
-        ax[i, j].get_yaxis().set_visible(False)
+    vutils.save_image(test_images.data,path)
 
-    for k in range(5*5):
-        i = k // 5
-        j = k % 5
-        ax[i, j].cla()
-        ax[i, j].imshow(test_images[k, 0].cpu().data.numpy(), cmap='gray')
 
-    label = 'Epoch {0}'.format(num_epoch)
-    fig.text(0.5, 0.04, label, ha='center')
-    plt.savefig(path)
-
-    if show:
-        plt.show()
-    else:
-        plt.close()
-
-def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
-    x = range(len(hist['D_losses']))
-
-    y1 = hist['D_losses']
-    y2 = hist['G_losses']
-
-    plt.plot(x, y1, label='D_loss')
-    plt.plot(x, y2, label='G_loss')
-
-    plt.xlabel('Iter')
-    plt.ylabel('Loss')
-
-    plt.legend(loc=4)
-    plt.grid(True)
-    plt.tight_layout()
-
-    if save:
-        plt.savefig(path)
-
-    if show:
-        plt.show()
-    else:
-        plt.close()
 
 # training parameters
 batch_size = 128
@@ -237,8 +197,8 @@ for epoch in range(train_epoch):
                                                               torch.mean(torch.FloatTensor(G_losses))))
     p = 'MNIST_DCGAN_results/Random_results/MNIST_DCGAN_' + str(epoch + 1) + '.png'
     fixed_p = 'MNIST_DCGAN_results/Fixed_results/MNIST_DCGAN_' + str(epoch + 1) + '.png'
-    show_result((epoch+1), save=True, path=p, isFix=False)
-    show_result((epoch+1), save=True, path=fixed_p, isFix=True)
+    save_result(fixed_p,isFix=True)
+    save_result(p,isFix=False)
     train_hist['D_losses'].append(torch.mean(torch.FloatTensor(D_losses)))
     train_hist['G_losses'].append(torch.mean(torch.FloatTensor(G_losses)))
     train_hist['per_epoch_ptimes'].append(per_epoch_ptime)
@@ -253,11 +213,3 @@ torch.save(G.state_dict(), "MNIST_DCGAN_results/generator_param.pkl")
 torch.save(D.state_dict(), "MNIST_DCGAN_results/discriminator_param.pkl")
 with open('MNIST_DCGAN_results/train_hist.pkl', 'wb') as f:
     pickle.dump(train_hist, f)
-
-show_train_hist(train_hist, save=True, path='MNIST_DCGAN_results/MNIST_DCGAN_train_hist.png')
-
-images = []
-for e in range(train_epoch):
-    img_name = 'MNIST_DCGAN_results/Fixed_results/MNIST_DCGAN_' + str(e + 1) + '.png'
-    images.append(imageio.imread(img_name))
-imageio.mimsave('MNIST_DCGAN_results/generation_animation.gif', images, fps=5)
